e883595b91e9a64b3f626f0cc2a0261f35364f2d
Evans <34720517+evansdai@users.noreply.github.com>
2026-01-02 21:32:06 +0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -255,7 +255,12 @@ require('lazy').setup({
   --
   -- Use `opts = {}` to automatically pass options to a plugin's `setup()` function, forcing the plugin to be loaded.
   --
-
+  {
+    'numToStr/Comment.nvim',
+    opts = {
+      -- add any options here
+    },
+  },
   -- Alternatively, use `config = function() ... end` for full control over the configuration.
   -- If you prefer to call `setup` explicitly, use:
   --    {
