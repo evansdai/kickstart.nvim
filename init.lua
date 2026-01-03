4ee939b3886a77fba28ba03207b2dcad766b767a
Evans <34720517+evansdai@users.noreply.github.com>
2026-01-04 05:23:50 +0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -1302,18 +1302,6 @@ require('lazy').setup({
       },
     },
   },
-  {
-    'yetone/avante.nvim',
-    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-    -- ⚠️ must add this setting! ! !
-    build = vim.fn.has 'win32' ~= 0 and 'powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false' or 'make',
-    event = 'VeryLazy',
-    version = false, -- Never set this value to "*"! Never!
-    ---@module 'avante'
-    ---@type avante.Config
-    opts = {
-      -- add any opts here
-      -- this file can contain specific instructions for your project
   -- {
   --   'yetone/avante.nvim',
   --   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
